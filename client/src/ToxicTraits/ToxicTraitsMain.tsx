cc40a75b88339bde3b67232866253094d49a30de
eloiclacomme <109964445+eloiclacomme@users.noreply.github.com>
2022-10-15 11:11:05 -0400

    moved ToxicUsers data

--- client/src/ToxicTraits/ToxicTraitsMain.tsx
+++ client/src/ToxicTraits/ToxicTraitsMain.tsx
@@ -3,7 +3,34 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import ToxicTraitsUser from './ToxicTraitsUser';
 import './ToxicTraits.css';
-import ToxicUsers from './TestToxicData';
+
+const ToxicUsers = [
+  {
+    name: 'Michael Arush',
+    image:
+      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
+  },
+  {
+    name: 'Arush Zu',
+    image:
+      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
+  },
+  {
+    name: 'Vincent Arush Tnecniv',
+    image:
+      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
+  },
+  {
+    name: 'Arush Hsura',
+    image:
+      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
+  },
+  {
+    name: 'Michael Arush',
+    image:
+      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
+  },
+];
 
 function ToxicTraitsMain() {
   return (
