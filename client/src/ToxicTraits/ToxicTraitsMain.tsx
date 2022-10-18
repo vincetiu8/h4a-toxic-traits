3fccfd8b783bf06eb7c966941ee9dee8b5303852
Arush Mehrotra <43364437+arush-mehrotra@users.noreply.github.com>
2022-10-18 16:25:58 -0400

    added api endpoints to user card page

--- client/src/ToxicTraits/ToxicTraitsMain.tsx
+++ client/src/ToxicTraits/ToxicTraitsMain.tsx
@@ -1,37 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import axios from 'axios';
 import ToxicTraitsUser from './ToxicTraitsUser';
-
-const ToxicUsers = [
-  {
-    name: 'Michael Arush',
-    image:
-      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
-  },
-  {
-    name: 'Arush Zu',
-    image:
-      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
-  },
-  {
-    name: 'Vincent Arush Tnecniv',
-    image:
-      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
-  },
-  {
-    name: 'Arush Hsura',
-    image:
-      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
-  },
-  {
-    name: 'Michael Arush',
-    image:
-      'https://www.seas.upenn.edu/~cis120/22fa/images/headshots/arushm.jpg',
-  },
-];
+import { getData } from '../util/api';
 
 function ToxicTraitsMain() {
+  const [users, setUsers] = useState<any>([]);
+
+  const fetchData = () => {
+    getData('profile/all').then((res) => {
+      console.log(res.data);
+      setUsers(
+        res.data.map(
+          (user: {
+            _id: string;
+            firstName: string;
+            lastName: string;
+            imageURL: string;
+          }) => ({
+            /* eslint no-underscore-dangle: 0 */
+            id: user._id,
+            name: `${user.firstName} ${user.lastName}`,
+            image: user.imageURL,
+          }),
+        ),
+      );
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
     <Box
       sx={{
@@ -43,9 +44,9 @@ function ToxicTraitsMain() {
       className="traitsMainContainer"
     >
       <Grid container spacing={2}>
-        {ToxicUsers.map(({ name, image }) => (
+        {users.map((user: { name: string; image: string }) => (
           <Grid item xs={4}>
-            <ToxicTraitsUser name={name} image={image} />
+            <ToxicTraitsUser name={user.name} image={user.image} />
           </Grid>
         ))}
       </Grid>
